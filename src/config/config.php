880323eecefda4c89ca439c304f76fcd7cc9670d
Chris Reid <chrisreiduk@gmail.com>
2015-09-17 15:52:07 +0100

    Add warning level

--- src/config/config.php
+++ src/config/config.php
@@ -4,10 +4,11 @@ return [
 
 	'slack' => [
 	
-		'enabled'  => env('APP_DEBUG'),
-		'token'    => env('SLACK_TOKEN'),
-	    'channel'  => env('SLACK_CHANNEL'),
-	    'username' => env('SLACK_USERNAME'),
+		'enabled'  		=> env('APP_DEBUG'),
+		'warning_level' => env('SLACK_WARNING_LEVEL', \Monolog\Logger::WARNING),
+		'token'    		=> env('SLACK_TOKEN'),
+	    'channel'  		=> env('SLACK_CHANNEL'),
+	    'username'		=> env('SLACK_USERNAME'),
 
 	],
 
