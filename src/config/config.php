dc2f0705b38eab347f0b368a1cd56b0c4cf796ff
Chris Reid <chrisreiduk@gmail.com>
2015-09-17 16:27:08 +0100

    Improved config options

--- src/config/config.php
+++ src/config/config.php
@@ -4,11 +4,11 @@ return [
 
 	'slack' => [
 	
-		'enabled'  		=> env('APP_DEBUG'),
-		'warning_level' => env('SLACK_WARNING_LEVEL', \Monolog\Logger::WARNING),
-		'token'    		=> env('SLACK_TOKEN'),
-	    'channel'  		=> env('SLACK_CHANNEL'),
-	    'username'		=> env('SLACK_USERNAME'),
+		'enabled'  		=> env('LOGIFIER_SLACK_ENABLED', !env('APP_DEBUG')),
+		'warning_level' => env('LOGIFIER_SLACK_WARNING_LEVEL', \Monolog\Logger::WARNING),
+		'token'    		=> env('LOGIFIER_SLACK_TOKEN'),
+	    'channel'  		=> env('LOGIFIER_SLACK_CHANNEL'),
+	    'username'		=> env('LOGIFIER_SLACK_USERNAME'),
 
 	],
 
